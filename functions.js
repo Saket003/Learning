27cc37c6d6ac98a94def69e1cd31360a8a687cfe
Saket003 <97734388+Saket003@users.noreply.github.com>
2022-07-12 11:42:04 +0530

    Master (#4)
    * Updated
    
    * Updates
    
    * Updates with drop-down and click response
    
    * Updates

--- functions.js
+++ functions.js
@@ -2,8 +2,15 @@ function Found() {
     document.getElementById("find").innerHTML = "Here's a cookie 🍪";
 }
 function Reveal1() {
-    document.getElementById("playlist").innerHTML = "Playlist <ul><li>Noita</li> <li>Undermine</li> <li>Smash Bros</li> <li>Breath of the Wild</li> <li>Binding of Isaac</li> <li>Subnautica</li> <li>Don't Starve</li> <li>Hollow Knight</li> <li>Valo</li> <li>Cuphead</li> <li>SMO and SMM</li> <li>Legends Arceus</li> <li>Elden Ring</li> <li>Genshin</li> <li>Silksong</li> </ul>";
+    if(document.getElementById("playlist").innerHTML == "Playlist"){
+        document.getElementById("playlist").innerHTML = "Playlist <ul><li>Noita</li> <li>Undermine</li> <li>Smash Bros</li> <li>Breath of the Wild</li> <li>Binding of Isaac</li> <li>Subnautica</li> <li>Don't Starve</li> <li>Hollow Knight</li> <li>Valo</li> <li>Cuphead</li> <li>SMO and SMM</li> <li>Legends Arceus</li> <li>Elden Ring</li> <li>Genshin</li> <li>Silksong</li> </ul>";
+    }
+    else{
+        document.getElementById("playlist").innerHTML = "Playlist";
+    }
 }
+
+//check use var instead
 function Reveal2() {
     document.getElementById("binge").innerHTML = "Binge:<ul><li>That 70s show</li><li>HIMYM</li><li>Friends</li><li>The Office</li><li>BBT</li><li>2broke</li><li>Agents of Shield</li><li>Gotham</li><li>All DCU/marvel</li><li>B99</li><li>Superstore</li><li>Man with a plan</li><li>Good place</li><li>Two and a half men</li><li>Lucifer</li></ul>";
 }
